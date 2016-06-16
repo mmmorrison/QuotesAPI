4ee8b01c7ead27191f8ad056fce4879242029c5d
Maria <mmorrison1978@gmail.com>
2016-06-16 10:02:26 -0600

    wired up routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('../db/knex.js');
+
+function Quotes() {
+  return knex('quotes');
+};
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+route.get('/all', function (req, res, next) {
+  Quotes().select().then(function(results) {
+    res.json(results)
+  })
 });
 
 module.exports = router;
