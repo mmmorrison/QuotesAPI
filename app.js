8c9f7d6a74ff0aed2c8512bd8bb217cddeca9d3f
Maria <mmorrison1978@gmail.com>
2016-06-16 09:41:20 -0600

    wired up routes

--- app.js
+++ app.js
@@ -1,13 +1,38 @@
-var express = require('express'),
-  app = express();
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var express = require('express');
+var router = express.Router();
+var knex = require('../db/knex.js');
 
-// WHEN a user visits the homepage (like http://localhost:3000)
-app.get("/", function (req, res) {
-  // THEN send back the response: 'Hello World'
-  res.send("Hello World");
+// var all = require('./routes/all');
+
+var app = express();
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
+app.use('/all', all);
+
+function All() {
+  return knex('quotes')
+}
+
+router.get('/', function(req, res, next) {
+  All().select().then(function(results) {
+    res.json(results);
+  })
+});
 // Start the server and make our web app available on http://localhost:3000
 app.listen(3000, function () {
   console.log("Starting a server on localhost:3000");
 });
+
+module.exports = app;
+module.exports = router;
