ef9407646e283babc04f6cbc6d2eaac059bd1006
Maria <mmorrison1978@gmail.com>
2016-06-17 10:00:49 -0600

    updated

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex.js');
+var id = Math.floor(Math.random() * 7);
 
 function Quotes() {
   return knex('quotes');
@@ -17,10 +18,22 @@ router.get('/all', function (req, res, next) {
   })
 });
 
-router.get('/random', function (req, res, next) {
-  var id = Math.floor(Math.random() * quotes.length);
-  var q = quotes[id];
-  res.json(q)
+router.get('/random/:' + id, function (req, res, next) {
+  Quotes().where('id', req.params.id).first().then(function(results) {
+    res.json(results)
+  })
 })
 
 module.exports = router;
+// router.get('/:id', function(req, res, next) {
+//   getDB().where('id', req.params.id).first().then(function(results) {
+//     getReviews().where('rest_id', req.params.id).then(function(review_results) {
+//       getHood().then(function(hood_results) {
+//         getEmployees().where('rest_id', req.params.id).then(function(employees) {
+//           res.render('restaurants/show', {restaurants: results, reviews: review_results, neighborhoods: hood_results,
+//             employees: employees});
+//         })
+//       })
+//     })
+//   })
+// });
